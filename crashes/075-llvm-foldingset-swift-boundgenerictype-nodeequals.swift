34aebe973d11286edb0cf08a8bc622157a0914a6
practicalswift <practicalswift@users.noreply.github.com>
2014-09-25 16:10:10 +0200

    Minimized test case.

--- crashes/075-llvm-foldingset-swift-boundgenerictype-nodeequals.swift
+++ crashes/075-llvm-foldingset-swift-boundgenerictype-nodeequals.swift
@@ -2,55 +2,13 @@
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // Test case found by fuzzing
 
-func f<T : BooleanType>(b: T) {
+class k<g>: d {
+    var f: g
+    init(f: g) {
+        self.f = f
+        l. d {
+    typealias i = l
+    typealias j = j<i<l>, i>
 }
-f(true as BooleanType)
-protocol A {
-    typealias B
-    func b(B)
-}
-struct X<Y> : A {
-    func b(b: X.Type) {
-    }
-}
-func prefix(ass func c() { }
-}
-(b() as a).dynamicType.c()
-struct c<d : SequenceType> {
-    var b: d
- D.C == E> {
-}
-import Foundation
-class Foo<T>: NSObject {
-    var foo: T
-    init(foo: T) {
-        self.foo = foo
-        super. b {
-    typealias d = h
-    typealias e = a<c<h>, d>
-}
-class a {
-    typealias b = b
-}
-func ^(a: BooleanType, Bool) -> Bool {
-    return !(a)
-}
-func some<S: SequenceType, T where Optional<T> == S.Generator.Element>(xs : S) -> T? {
-    for (mx : T?) in xs {
-        if let x = mx {
-            return x
-        }
-    }
-    return nil
-}
-let xs : [Int?] = [nil, 4, nil]
-println(some(xs))
-tocol f {
-   class func i()
-}
-class d: f{  class func i {}
-func a<T>() {
-    enum b {
-        case c
-    }
-}
+class j {
+    typealias d = d
