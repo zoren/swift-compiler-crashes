72f29c0a5855f5f77cf4bdde3a799c928ec8eec7
practicalswift <practicalswift@users.noreply.github.com>
2014-09-26 16:40:47 +0200

    Merged test cases 001 and 020-void.swift (same issue). Renamed 173 to 020 to fill gap.

--- crashes/001-swift-typeloc-iserror.swift
+++ crashes/001-swift-typeloc-iserror.swift
@@ -1,5 +1,13 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/17157251
 
+// Case #1
 protocol a : a {
 }
+// Case #2 (appears to trigger same bug)
+class A : A {
+}
+class B : C {
+}
+typealias C = B
--- crashes/020-void.swift
+++
@@ -1,9 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
-// http://www.openradar.me/17157251
-
-class A: A {
-}
-class B : C {
-}
-typealias C = B
