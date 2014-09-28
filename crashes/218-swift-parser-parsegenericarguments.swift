121bab3929033fe1eca075099c46778556fb543e
practicalswift <practicalswift@users.noreply.github.com>
2014-09-28 10:47:34 +0200

    Added test cases found by fuzzing.

---
+++ crashes/218-swift-parser-parsegenericarguments.swift
@@ -0,0 +1,62 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+class a {
+    typealias b = b
+}
+func d() -> String {
+    return 1
+k f {
+    typealias c
+}
+class g<i{
+}
+d(j i)
+class h {
+    typealias i = i
+}
+b
+protocol c : b { func b
+a=1 as a=1
+}
+func ^(r: l, k) -> k {
+   ? {
+    h (s : t?) q u {
+        g let d = s {
+            p d
+        }
+    }
+    e}
+let u : [Int?] = [n{
+    c v: j t.v == m>(n: o<t>) {
+    }
+}
+class r {
+    ty
+protocol f {
+  i []
+}
+func f<g>() -> (g, g -> g) -> g
+func o() as o).m.k()
+func p(k: b) -> <i>(() -> i) -> b {
+  n { o f "\(k): \(o())" }
+}
+struct d<d : n, o:j n {
+    l p
+}
+protocol o : o {
+}
+func o<
+d = i
+}
+class d<j :<T : A> {
+    let h: T
+    let i: T.E
+}
+protocol C {
+    typealias F
+    func g<T where T.E == F>(f: B<T>)
+}
+struct D : C {
+    typealias F = Int
