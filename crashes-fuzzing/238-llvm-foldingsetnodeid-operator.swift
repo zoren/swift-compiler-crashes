379badce276e0f5da1545e0da4be0cdf6fc2fb63
practicalswift <practicalswift@users.noreply.github.com>
2014-09-29 18:07:50 +0200

    Added two crash cases found by fuzzing.

---
+++ crashes-fuzzing/238-llvm-foldingsetnodeid-operator.swift
@@ -0,0 +1,52 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+protocol a {
+  typealias d
+  typealias e = d
+  typealias f = d
+}ocol c {
+    typealias g
+}
+v d: Int =  { c, b in
+}(f, e)
+struct c<d, e: b
+}
+func a<d>() -> [c<d>] {
+    return []
+}
+protocol A {
+    typealias E
+}
+struct B<T : A> {
+    let h: T
+    : e, g: e where g.h == f.h> {
+}
+protocol e {
+    typealias h
+}
+enum S<T> : P {
+    func f<T>() -> T -> T {
+        return { x in x }
+    }
+}
+protocol P {
+    func f<T>()(T) -> T
+}
+func f<T : BooleanType>(b: T) {
+}
+f(true as BooleanType)
+class a {
+    type {
+    var _ = i() {
+    }
+}
+class a<f : b, g : b where f.d == g> {
+}
+protocol b {
+    typealias d
+    typealias e
+}
+struct c<h : b> : b {
+    typec>: NSO
