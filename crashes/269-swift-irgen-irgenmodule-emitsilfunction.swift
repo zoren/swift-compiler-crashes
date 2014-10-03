fa9f56d0e554d1d4269af3c4d80ccd74d6630ce4
practicalswift <practicalswift@users.noreply.github.com>
2014-10-03 21:45:46 +0200

    Typo: added missing newline.

--- crashes/269-swift-irgen-irgenmodule-emitsilfunction.swift
+++ crashes/269-swift-irgen-irgenmodule-emitsilfunction.swift
@@ -1,6 +1,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // https://twitter.com/AndyIbanezK/status/517759842354995200
+
 enum A : String {
     case b = ""
 }
