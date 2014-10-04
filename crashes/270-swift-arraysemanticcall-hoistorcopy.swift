65e9127453e0da864047f0adc37f65b465d2f9d1
practicalswift <practicalswift@users.noreply.github.com>
2014-10-04 20:05:45 +0200

    Added test cases: * 270-swift-arraysemanticcall-hoistorcopy.swift * 271-swift-irgen-irgenfunction-emittypemetadataref.swift

---
+++ crashes/270-swift-arraysemanticcall-hoistorcopy.swift
@@ -0,0 +1,10 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// https://devforums.apple.com/message/1051862
+
+func a() {
+    var b = [[Int]]()
+    for c in 0..<1 {
+        b[c][c] = 1
+    }
+}
---
+++ crashes/271-swift-irgen-irgenfunction-emittypemetadataref.swift
@@ -0,0 +1,10 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// https://devforums.apple.com/message/1051132
+
+import CoreData
+class A : NSManagedObject {
+    func b<T: A>() -> [T] {
+        return [T]()
+    }
+}
