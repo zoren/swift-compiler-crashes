7adaa244581d9691cafe7f940ff94e0d08050f62
practicalswift <practicalswift@users.noreply.github.com>
2014-10-07 21:57:23 +0200

    Updated hall of fame :-)

--- crashes/272-llvm-irbuilder-createcall.swift
+++ crashes/272-llvm-irbuilder-createcall.swift
@@ -1,9 +1,11 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/fluidsonic (Marc Knaup)
+
 class A {
     class func a() -> Self {
         return b(self.dynamicType)
     }
 }
-
 
 func b<T>(t: AnyObject.Type) -> T! {
     return nil
