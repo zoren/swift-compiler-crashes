41a3cede099e118e52335eba1e449aaa988b049c
practicalswift <practicalswift@users.noreply.github.com>
2014-10-12 14:35:34 +0200

    Re-added crashes-duplicates/264-isvalididentifiercontinuationcodepoint.swift

---
+++ crashes-duplicates/264-isvalididentifiercontinuationcodepoint.swift
@@ -0,0 +1,16 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+import Fou    init(b: c) {
+  ypealias E
+}
+struct B<T  typealias F = Int
+    func g<T where T.E == F>(f: B<T>) {
+    }
+}
+func b(c) -> <d>(() -> d) {
+}
+impoience ire : C)  c
+    }
+}
