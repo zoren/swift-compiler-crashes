cc5e8b43b632f0d8f1940a086a33ce917a99f895
practicalswift <practicalswift@users.noreply.github.com>
2014-10-13 21:51:20 +0200

    Added crash case crashes/322-swift-mangle-mangler-manglenominaltype.swift
    Thanks to @robrix for find it! :-)

---
+++ crashes/322-swift-mangle-mangler-manglenominaltype.swift
@@ -0,0 +1,7 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/18637242
+
+protocol P {
+    init(x: Int? = nil)
+}
