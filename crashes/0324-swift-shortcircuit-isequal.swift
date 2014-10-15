a3daa141cb7d523b4c32174809421da123794993
Martijn Walraven <martijn@martijnwalraven.com>
2014-10-15 13:19:30 +0200

    Add case crashes/0324-swift-shortcircuit-isequal

---
+++ crashes/0324-swift-shortcircuit-isequal.swift
@@ -0,0 +1,11 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by http://github.com/martijnwalraven (Martijn Walraven)
+// rdar://18662915
+
+import Foundation
+
+class Test: NSObject {
+  override func isEqual(object: AnyObject?) -> Bool {
+    return false && super.isEqual(object)
+  }
+}
