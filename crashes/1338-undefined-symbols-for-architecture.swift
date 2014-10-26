276531e5495ce3d34935f396d54ff53bcbc00b04
practicalswift <practicalswift@users.noreply.github.com>
2014-10-26 20:12:32 +0100

    Added test case: 1338-undefined-symbols-for-architecture.swift

---
+++ crashes/1338-undefined-symbols-for-architecture.swift
@@ -0,0 +1,13 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// rdar://18706056
+
+import Foundation
+
+@objc protocol A {
+   var a : String { get } 
+}
+
+class B : A {
+    @NSManaged var a : String
+}
