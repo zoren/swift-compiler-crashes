2fe068d0d2b4c0e97aba6c9e2fe798cfb1be71e4
practicalswift <practicalswift@users.noreply.github.com>
2014-10-28 21:57:07 +0100

    Added crash case 1339-phi-node-operands-not-same-as-result.swift

---
+++ crashes/1339-phi-node-operands-not-same-as-result.swift
@@ -0,0 +1,19 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/18756378
+
+import Foundation
+
+enum A<T : AnyObject> {
+    case E(T)
+}
+class B {
+}
+func c(d : A<B>) {
+    switch (d) {
+        case .E(var e):
+            println("")
+        default:
+            println("")
+    }
+}
