f42b877661f7ed08d68801058ee4b6e976ac32d2
practicalswift <practicalswift@users.noreply.github.com>
2014-11-02 10:23:46 +0100

    Added test case 1646-super-inside-an-autoclosure.swift

---
+++ crashes/1646-super-inside-an-autoclosure.swift
@@ -0,0 +1,9 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+
+import CoreData
+class A : NSManagedObjectContext {
+    override func save(a: NSErrorPointer) -> Bool {
+        return self.hasChanges || super.save(a)
+    }
+}
