a6cf61f80dd3a283e3c59f7a2784f5b70f065ff6
practicalswift <practicalswift@users.noreply.github.com>
2014-11-04 19:50:39 +0100

    Minimized test case.

--- crashes/1647-no-stacktrace.swift
+++ crashes/1647-no-stacktrace.swift
@@ -2,22 +2,18 @@
 // Test case submitted to project by https://github.com/radex
 // rdar://18851497
 
-struct Struct {
-    private let array: [Int] = []
-    
-    func poke() {
+struct A {
+    private let b: [Int] = []
+    func a() {
     }
 }
-
-enum Enum {
-    case Case1
-    case Case2
+enum C {
+    case D
+    case E
 }
-
-typealias Alias = (Enum, Enum)
-
-func doSomething() {
-    let expr: (Alias, Struct)? = nil
-    let (expr2, stream2) = expr!
-    stream2.poke()
+typealias B = (C, C)
+func c() {
+    let d: (B, A)? = nil
+    let (e, f) = d!
+    f.a()
 }
