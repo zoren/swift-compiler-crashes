53f2531903c5063b7dc8c16a3efa8fdcd4554274
beltex <beltex@users.noreply.github.com>
2014-11-18 18:05:46 -0500

    Added compiler hang - 2260 - kinfo_proc alloc
    - In ref to https://github.com/practicalswift/swift-compiler-crashes/pull/34

---
+++ crashes/2260-kinfo_proc-alloc.timeout.swift
@@ -0,0 +1,9 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/beltex (beltex) &
+// https://github.com/practicalswift (practicalswift)
+//
+// https://github.com/practicalswift/swift-compiler-crashes/pull/34 
+
+import Darwin
+
+UnsafeMutablePointer<kinfo_proc>.alloc(1)
