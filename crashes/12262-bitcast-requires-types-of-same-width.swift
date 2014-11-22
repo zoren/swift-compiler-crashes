88c8a6bd5646cbad4e1a05a1e3e3a92b95f26fc7
practicalswift <practicalswift@users.noreply.github.com>
2014-11-22 23:36:18 +0100

    Added crash case 12262-bitcast-requires-types-of-same-width.swift

---
+++ crashes/12262-bitcast-requires-types-of-same-width.swift
@@ -0,0 +1,10 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// https://devforums.apple.com/message/1076229
+
+class A<T: AnyObject> {
+    weak var a : T?
+}
+protocol B : class {
+}
+print((Array<A<B>>()[0] as A<B>).a)
