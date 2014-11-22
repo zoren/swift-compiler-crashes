2093ef07bc537d949cc6529381dfd502d77461e9
airspeedswift <ben@airspeedvelocity.net>
2014-11-22 07:06:46 -0500

    Create 02261-destructing-pair-enum-tuple.swift

---
+++ crashes/02261-destructing-pair-enum-tuple.swift
@@ -0,0 +1,17 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/airspeedswift (airspeedswift)
+// https://twitter.com/AirspeedSwift/status/535452613077401601
+// rdar://19067567
+
+enum E {
+  case A
+}
+
+func f() -> (t: (Int,Int),e: E) {
+  return ((0,0),.A)
+}
+
+func h() {
+  let (t,e) = f()
+  print(e)
+}
