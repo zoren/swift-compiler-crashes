03007457572200e0f9a29cb0fd28c1afe7d96189
practicalswift <practicalswift@users.noreply.github.com>
2014-12-20 13:39:58 +0100

    Added test case 22723-swift-lowering-silgenfunction-emitexprinto.swift

---
+++ crashes/22723-swift-lowering-silgenfunction-emitexprinto.swift
@@ -0,0 +1,15 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/19204410
+
+infix operator |> {
+}
+func |> <g,c> (g: g, e: g -> c) -> c {
+}
+func e(a: Int, c: Int) -> [CChar] {
+    for e in a..<c {
+    }
+}
+func h(a: Int, c: Int) -> String? {
+    return e(a, c) |> String.fromCString
+}
