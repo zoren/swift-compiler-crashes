2db6ac2f6a9abfcdcc2e988f554d0858afa2f9d5
practicalswift <practicalswift@users.noreply.github.com>
2014-12-20 13:13:18 +0100

    Added crash case 22524-collectfullname.swift

---
+++ crashes/22524-collectfullname.swift
@@ -0,0 +1,13 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// https://twitter.com/zneakr/status/544581388817928192
+
+protocol c {
+    init()
+}
+class b: c {
+    required init() {
+    }
+}
+let a: c.Type = b.self
+a()
