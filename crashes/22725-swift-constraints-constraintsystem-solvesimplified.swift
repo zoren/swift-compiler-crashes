9accbf1ae07db7a09b3d9fa0c93f8456f2ee15ff
practicalswift <practicalswift@users.noreply.github.com>
2014-12-25 11:32:45 +0100

    Added 22725-swift-constraints-constraintsystem-solvesimplified.swift (thanks to @robrix for finding! :-))

---
+++ crashes/22725-swift-constraints-constraintsystem-solvesimplified.swift
@@ -0,0 +1,8 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/19343997
+
+func b<T>(String -> (T String)?
+func |<T>(c: String -> (T String)?
+a:String > (())) -> String -> (T? String)?
+b("" | "" | "" | "")
