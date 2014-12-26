ff4f6aa9f2962203adda79c5a01be61beedb4f31
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 18:46:59 +0100

    crashes-fuzzing/03732-swift-modulefile-gettype.swift

---
+++ crashes-duplicates/03732-swift-modulefile-gettype.swift
@@ -0,0 +1,20 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+{
+{
+{
+struct S{
+struct Q{
+func a(){
+{
+{
+struct B
+{
+func a
+{
+if a{
+class
+case c,
+()
