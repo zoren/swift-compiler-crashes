126783f3844178aaaa3cbccc6643d315ce6ad351
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 18:47:46 +0100

    Mark as duplicate.

--- crashes-fuzzing/03752-vtable.swift
+++
@@ -1,5 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
-// Test case found by fuzzing
-
-struct S<T where a=e{let a{func b<I:A
