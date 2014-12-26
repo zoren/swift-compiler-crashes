fba4934b1ec937d31990c29b469f2b505797b405
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 11:54:13 +0100

    Added commas.

--- crashes/22725-swift-constraints-constraintsystem-solvesimplified.swift
+++ crashes/22725-swift-constraints-constraintsystem-solvesimplified.swift
@@ -2,7 +2,7 @@
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // http://www.openradar.me/19343997
 
-func b<T>(String -> (T String)?
-func |<T>(c: String -> (T String)?
-a:String > (())) -> String -> (T? String)?
+func b<T>(String -> (T, String)?
+func |<T>(c: String -> (T, String)?
+a:String > (())) -> String -> (T?, String)?
 b("" | "" | "" | "")
