30c131d9d9f0c10176e4d296e16402c5c7297c71
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 18:47:48 +0100

    Mark as duplicate.

--- crashes-fuzzing/03859-resolvetypedecl.swift
+++
@@ -1,5 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
-// Test case found by fuzzing
-
-enum S<h where T:S)enum S{var d=B)struct B<f:S
