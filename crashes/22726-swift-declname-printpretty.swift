7f506b2ab95e27d1d69204ed8c619c362f14b8ef
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 18:59:38 +0100

    Added 22726-swift-declname-printpretty.swift (thanks to @robrix for finding! :-))

---
+++ crashes/22726-swift-declname-printpretty.swift
@@ -0,0 +1,8 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/19349215
+
+extension Optional {
+    struct S {
+    }
+}
