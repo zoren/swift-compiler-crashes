d06f87be65789167555a02c2318eadb68fc8e426
practicalswift <practicalswift@users.noreply.github.com>
2014-12-26 18:47:52 +0100

    Mark as duplicate.

--- crashes-fuzzing/04165-swift-declcontext-lookupqualified.swift
+++
@@ -1,5 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
-// Test case found by fuzzing
-
-class A{deinit{class C<T where I:t{let a=V
