f0432c2d9b74c0f36fb0a12b18ad18c14c468e4d
practicalswift <practicalswift@users.noreply.github.com>
2014-12-27 23:16:44 +0100

    Minimized test case.

--- crashes/22776-lambda-testing-optional-enum-field.swift
+++ crashes/22776-lambda-testing-optional-enum-field.swift
@@ -1,16 +1,10 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/zneak (zneak)
 
-enum Foo {
-	case Bar
+enum a {
+    case b
 }
-
-struct FooWrapper {
-	let foo: Foo?
-	init(foo: Foo?) {
-		self.foo = foo
-	}
+struct c {
+    let d: a?
 }
-
-let foo = FooWrapper(foo: .Bar)
-let x: () -> Bool = { foo.foo == .Bar }
+let d = c(d: .b)
