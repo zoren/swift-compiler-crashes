1807982223f3347751490b00452ad568caf7d51b
practicalswift <practicalswift@users.noreply.github.com>
2014-12-27 23:26:36 +0100

    Simplified test case.

--- crashes/00271-swift-irgen-irgenfunction-emittypemetadataref.swift
+++ crashes/00271-swift-irgen-irgenfunction-emittypemetadataref.swift
@@ -2,8 +2,8 @@
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // https://devforums.apple.com/message/1051132
 
-import CoreData
-class A : NSManagedObject {
+import Foundation
+class A : NSObject {
     func b<T: A>() -> [T] {
         return [T]()
     }
