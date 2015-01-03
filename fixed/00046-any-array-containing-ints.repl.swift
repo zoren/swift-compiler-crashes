2721f711721d39270ff4ef87dcb75d9b971a86d7
practicalswift <practicalswift@users.noreply.github.com>
2015-01-03 21:56:55 +0100

    Credit where credit is due :-)

--- fixed/00046-any-array-containing-ints.repl.swift
+++ fixed/00046-any-array-containing-ints.repl.swift
@@ -1,4 +1,4 @@
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case submitted to project by https://github.com/rnapier (Rob Napier)
 // http://www.openradar.me/17916262
 
 let n1: [Any] = [1]
