4df1aa80c972c7beaa6eceaf9ca0bfd95412deb8
practicalswift <practicalswift@users.noreply.github.com>
2015-01-05 00:38:33 +0100

    Normalized.

--- crashes/23172-lowering-silgenfunction-emitapplyexpr.swift
+++ crashes/23172-lowering-silgenfunction-emitapplyexpr.swift
@@ -1,11 +1,13 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/rustedivan (Ivan Milles)
+
 func f() {
-	var n = 0
-	class A {
-		func g() {
-			n = 1			// This kills the swiftc
-		}
-	}
-	
-	let a = A()
-	a.g()
+    var n = 0
+    class A {
+        func g() {
+            n = 1
+        }
+    }
+    let a = A()
+    a.g()
 }
