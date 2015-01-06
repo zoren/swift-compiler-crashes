d9c7d0490b9f8035eb7eb3669146c8ddb37d3aed
practicalswift <practicalswift@users.noreply.github.com>
2015-01-06 01:30:05 +0100

    Added URLs.

--- crashes/23173-llvm-getelementptrinst-create.swift
+++ crashes/23173-llvm-getelementptrinst-create.swift
@@ -1,5 +1,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/airspeedswift (airspeedswift)
+// https://twitter.com/AirspeedSwift/status/552120894173483008
+// http://stackoverflow.com/questions/27779473/how-to-use-a-protocol-with-optional-class-methods-in-an-extension-with-generic-i
 
 import Foundation
 @objc protocol P {
