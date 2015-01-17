079c2fa2874255b2f9828c0aaed90b825fdfa46b
practicalswift <practicalswift@users.noreply.github.com>
2015-01-17 11:04:05 +0100

    Added info about similar crash case.

--- crashes/00269-swift-irgen-irgenmodule-emitsilfunction.swift
+++ crashes/00269-swift-irgen-irgenmodule-emitsilfunction.swift
@@ -1,6 +1,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // https://twitter.com/AndyIbanezK/status/517759842354995200
+// http://www.openradar.me/19472840 (duplicate)
 
 enum A : String {
     case b = ""
