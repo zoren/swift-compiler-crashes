3b81c2c46470d56c7773dc32e0e8b4a1900a460a
practicalswift <practicalswift@users.noreply.github.com>
2015-01-17 13:20:22 +0100

    Simplify crash case.

--- crashes/23639-llvm-jit-runfunction.runtime.swift
+++ crashes/23639-llvm-jit-runfunction.runtime.swift
@@ -3,12 +3,12 @@
 // http://www.openradar.me/19423783
 
 protocol A {
-    var a: Float {
+    var a: Int {
         get
     }
 }
 class B: A {
-    let a: Float = 42
+    let a = 42
 }
 let b = B()
 let c = [b]
