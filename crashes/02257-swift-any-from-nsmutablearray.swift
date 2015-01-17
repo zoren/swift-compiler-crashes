e17ba5eddc5612ab9d087545cf491a4c10a3355b
practicalswift <practicalswift@users.noreply.github.com>
2015-01-17 11:04:42 +0100

    Added info about similar crash case.

--- crashes/02257-swift-any-from-nsmutablearray.swift
+++ crashes/02257-swift-any-from-nsmutablearray.swift
@@ -1,5 +1,6 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/mattdaw (Matt Daw)
+// http://www.openradar.me/19346816 (duplicate)
 
 import Foundation
 var a: NSMutableArray = []
