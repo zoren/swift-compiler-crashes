d451d32df90d4cdd2ed65fdefa29d406d4a74ad5
practicalswift <practicalswift@users.noreply.github.com>
2015-01-17 13:56:24 +0100

    Added credits for crash #22728 to @juangamnik – thanks! :-)

--- crashes/22728-swift-irgen-typeconverter-getexemplararchetype.swift
+++ crashes/22728-swift-irgen-typeconverter-getexemplararchetype.swift
@@ -1,11 +1,13 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/juangamnik (juangamnik)
+
 import Foundation
-
-class A: NSObject {}
-
-class B {}
-
+class A : NSObject {
+}
+class B {
+}
 extension A {
-    func f<T: B>() -> [T]? {
+    func f<T : B>() -> [T]? {
         return nil
     }
 }
