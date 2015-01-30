9c08659d8e1feff40e5f3fce92d549d5856ea03c
practicalswift <practicalswift@users.noreply.github.com>
2015-01-30 23:15:51 +0100

    Added link to same issue independently discovered by @airspeedswift :-)

--- crashes/22722-swift-irgen-protocolinfo-getconformance.swift
+++ crashes/22722-swift-irgen-protocolinfo-getconformance.swift
@@ -1,6 +1,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/rnapier (Rob Napier)
 // https://twitter.com/cocoaphony/status/546068161372905472
+// https://twitter.com/AirspeedSwift/status/561285177776037888 (same issue)
 
 protocol p {
 }
