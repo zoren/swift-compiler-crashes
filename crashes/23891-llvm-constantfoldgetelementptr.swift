e53e9a08cace686a00b2d1db680cfe63bb026650
practicalswift <practicalswift@users.noreply.github.com>
2015-02-01 23:15:48 +0100

    Add 23891-llvm-constantfoldgetelementptr.swift (thanks @airspeedvelocity! :-))

---
+++ crashes/23891-llvm-constantfoldgetelementptr.swift
@@ -0,0 +1,8 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/airspeedswift (airspeedswift)
+// https://twitter.com/AirspeedSwift/status/562010687334264832
+
+enum E<T> {
+    case A(T)
+    case B(Int)
+}
