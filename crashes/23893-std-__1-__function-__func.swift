82d8a430c85258e52e5931dcb64c672970a9ea0b
Robert Widmann <devteam.codafi@gmail.com>
2015-02-04 08:54:37 -0700

    Crash on partial apply of protocol class func

---
+++ crashes/23893-std-__1-__function-__func.swift
@@ -0,0 +1,23 @@
+protocol Arbitrary {
+    class func shrink(Self) -> [Self]
+}
+
+struct Gen<A> {}
+
+extension Bool : Arbitrary {    
+    public static func shrink(x : Bool) -> [Bool] {
+        if x {
+            return [false]
+        }
+        return []
+    }
+}
+
+func forAll<A : Arbitrary>(g : Gen<A>) {
+    return forAllShrink(A.shrink)
+}
+
+func forAllShrink<A : Arbitrary>(shrinker: A -> [A]) {
+    return fatalError("undefined")
+}
+
