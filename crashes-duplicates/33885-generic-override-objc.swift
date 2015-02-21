2cd16267129dae4f91f52cdccb6c8e99e1ffe830
Félix Cloutier <felix@felixcloutier.com>
2015-02-21 11:04:53 -0500

    Removed duplicate since it never was committed to practicalswift's repository

--- crashes-duplicates/33885-generic-override-objc.swift
+++
@@ -1,9 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/zneak (zneak)
-
-import Foundation
-
-class B<X>: NSObject {
-	let x: X
-	init(x: X) { self.x = x }
-}
