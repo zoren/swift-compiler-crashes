250b62d63ee3bb4efa3e441ec4bebc53a2368da6
practicalswift <practicalswift@users.noreply.github.com>
2015-07-25 10:12:39 +0200

    Rename and minimize.

--- crashes/25962-swift-ArchetypeBuilder-getAllArchetypes.swift
+++ crashes/25962-swift-ArchetypeBuilder-getAllArchetypes.swift
@@ -1,21 +1,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/adocyn (adocyn)
 
-extension CollectionType where Generator.Element: Equatable, Index.Distance == Int, Index == Int {
-
-   func findIndex<T where T == Generator.Element>(element: T) -> Int? {
-
-       var indexToFind:Int?
-
-       for i:Int in self.indices {
-
-           if element == self[i] {
-
-               indexToFind = i
-               break
-           }
-       }
-
-       return indexToFind
-   }
+extension CollectionType {
+    func f<T where T=Generator>() {
+    }
 }
