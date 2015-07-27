4d471b4e7f3483f534dd511323c3afa22831ba3b
Valeriy Van <github@w7software.com>
2015-07-28 00:42:41 +0300

    Swift Compiler Segmentation Fault. Xcode 6.4, Xcode 7 beta 3.

---
+++ crashes/22722-swift-irgen-irgenmodule::emitsilfunction.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  SwiftSegmentationFault1
+//
+//  Created by Valeriy Van on 7/28/15.
+//  Copyright (c) 2015 Valeriy Van. All rights reserved.
+//
+
+class C {
+    lazy var stuff: [(title: String, image: String, action: (Void)->Void)] = {
+        [unowned self] in
+        return [ (title: "Visit now", image: "visit icon", action: self.visitNow),
+            (title: "Settings", image: "settings icon", action: self.settings),
+            (title: "Log out", image: "logout icon", action: self.logout) ]
+        }()
+
+    func f() {
+        stuff[0].action()
+    }
+
+    func visitNow()->Void {
+        print("visitNow\n")
+    }
+
+    func logout() {
+        print("logout\n")
+    }
+    
+    func settings() {
+        print("settings\n")
+    }
+}
