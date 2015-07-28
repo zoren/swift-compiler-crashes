1a5a5fb0aea075bea2fa4950edb5ddaa0f894042
practicalswift <practicalswift@users.noreply.github.com>
2015-07-28 23:08:18 +0200

    Minimize test case.

--- crashes/26297-swift-irgen-irgenmodule-emitsilfunction.swift
+++ crashes/26297-swift-irgen-irgenmodule-emitsilfunction.swift
@@ -2,26 +2,9 @@
 // Test case submitted to project by https://github.com/valeriyvan (Valeriy Van)
 
 class C {
-    lazy var stuff: [(title: String, image: String, action: (Void)->Void)] = {
-        [unowned self] in
-        return [ (title: "Visit now", image: "visit icon", action: self.visitNow),
-            (title: "Settings", image: "settings icon", action: self.settings),
-            (title: "Log out", image: "logout icon", action: self.logout) ]
-        }()
-
-    func f() {
-        stuff[0].action()
-    }
-
-    func visitNow()->Void {
-        print("visitNow\n")
-    }
-
-    func logout() {
-        print("logout\n")
-    }
-    
-    func settings() {
-        print("settings\n")
-    }
+   var d: [(c: String, a: String, e: () -> Void)] = {
+   }()
+   func f() {
+     d[0].e()
+   }
 }
