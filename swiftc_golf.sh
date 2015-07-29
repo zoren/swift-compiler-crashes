b3faae864151d197d46b568b78f38c8e323624ea
practicalswift <practicalswift@users.noreply.github.com>
2015-07-29 22:51:43 +0200

    Improve formatting.

--- swiftc_golf.sh
+++ swiftc_golf.sh
@@ -40,31 +40,31 @@ test_crash_case() {
         echo "· ✓ · ${escaped_source_code}"
     fi
 }
-                             # +-----+-----+-----+-----+-----+-----+----------------+
-                             # | len | 6.2 | 7b1 | 7b2 | 7b3 | 7b4 | Crash location |
-                             # +-----+-----+-----+-----+-----+-----+----------------+
-test_crash_case '&_'         # |   2 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(swift::Expr*) + 924
-test_crash_case '{&_'        # |   3 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(swift::Expr*) + 924
-test_crash_case '&(-_'       # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::matchTypes(swift::Type, swift::Type, swift::constraints::TypeMatchKind, unsigned int, swift::constraints::ConstraintLocatorBuilder) + 417
-test_crash_case '&[_?'       # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::Type::walk(swift::TypeWalker&) const + 81
-test_crash_case 'for{{'      # |   5 |  ✓  |  ✘  |  ✘  |  ✘  |  ✓  | –
-test_crash_case '()=()'      # |   5 |  ✓  |  ✘  |  ✘  |  ✘  |  ✘  | emitSimpleAssignment(swift::Lowering::SILGenFunction&, swift::SILLocation, swift::Expr*, swift::Expr*) + 1410
-test_crash_case '&[(-{'      # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::StreamPrinter::printText(llvm::StringRef) + 53
-test_crash_case '{&(&)'      # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ModuleFile::getDecl(llvm::Fixnum<31u, unsigned int>, llvm::Optional<swift::DeclContext*>) + 14041
-test_crash_case '({[({_'     # |   6 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | (anonymous namespace)::PreCheckExpression::walkToExprPre(swift::Expr*) + 541
-test_crash_case '{nil-{_?'   # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | void (anonymous namespace)::visitProtocols<swift::ConformanceLookupTable::addProtocols(swift::NominalTypeDecl*, llvm::ArrayRef<swift::TypeLoc>, swift::ConformanceLookupTable::ConformanceSource, …
-test_crash_case '{(_>"",{'   # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::TypeBase::isEqual(swift::Type) + 15
-test_crash_case '{_{[true'   # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ConformanceLookupTable::resolveConformances(swift::NominalTypeDecl*, swift::ProtocolDecl*, swift::LazyResolver*) + 743
-test_crash_case '&.f{}()do'  # |   9 |     |     |     |     |  ✘  | std::__1::__function::__func<swift::constraints::ConstraintSystem::simplifyType(swift::Type, llvm::SmallPtrSet<swift::TypeVariableType*, 16u>&)::$_6, std::__1::allocator<swift::constraints::…
-test_crash_case '&Range.T{'  # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::getTypeOfMemberReference(swift::Type, swift::ValueDecl*, bool, bool, swift::constraints::ConstraintLocatorBuilder, swift::DeclRefExpr const*, …
-test_crash_case '&_{Range?'  # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::addConstraint(swift::constraints::Constraint*, bool, bool) + 280
-test_crash_case '.{nil<{\n{' # |   9 |     |     |     |     |  ✘  | swift::ConformanceLookupTable::expandImpliedConformances(swift::NominalTypeDecl*, swift::DeclContext*, swift::LazyResolver*) + 496
-test_crash_case '{nil...{('  # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::simplifyMemberConstraint(swift::constraints::Constraint const&) + 416
-test_crash_case '&true{for{' # |  10 |     |     |     |     |  ✘  | swift::AbstractClosureExpr::setParams(swift::Pattern*) + 80
-test_crash_case 'Range>\n&_{' #|  10 |     |     |     |     |  ✘  | swift::ASTContext::Allocate(unsigned long, unsigned int, swift::AllocationArena) const + 110
-test_crash_case '[&{}false?' # |  10 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::solve(llvm::SmallVectorImpl<swift::constraints::Solution>&, swift::FreeTypeVariableBinding) + 66
-test_crash_case '{nil{true?' # |  10 |     |     |     |     |  ✘  | swift::constraints::ConstraintGraphNode::addConstraint(swift::constraints::Constraint*) + 109
-                             # +-----+-----+-----+-----+-----+-----+
+                              # +-----+-----+-----+-----+-----+-----+----------------+
+                              # | len | 6.2 | 7b1 | 7b2 | 7b3 | 7b4 | Crash location |
+                              # +-----+-----+-----+-----+-----+-----+----------------+
+test_crash_case '&_'          # |   2 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(swift::Expr*) + 924
+test_crash_case '{&_'         # |   3 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(swift::Expr*) + 924
+test_crash_case '&(-_'        # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::constraints::ConstraintSystem::matchTypes(swift::Type, swift::Type, swift::constraints::TypeMatchKind, unsigned int, swift::constraints::ConstraintLocatorBuilder) + 417
+test_crash_case '&[_?'        # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::Type::walk(swift::TypeWalker&) const + 81
+test_crash_case 'for{{'       # |   5 |  ✓  |  ✘  |  ✘  |  ✘  |  ✓  | –
+test_crash_case '()=()'       # |   5 |  ✓  |  ✘  |  ✘  |  ✘  |  ✘  | emitSimpleAssignment(swift::Lowering::SILGenFunction&, swift::SILLocation, swift::Expr*, swift::Expr*) + 1410
+test_crash_case '&[(-{'       # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::StreamPrinter::printText(llvm::StringRef) + 53
+test_crash_case '{&(&)'       # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ModuleFile::getDecl(llvm::Fixnum<31u, unsigned int>, llvm::Optional<swift::DeclContext*>) + 14041
+test_crash_case '({[({_'      # |   6 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | (anonymous namespace)::PreCheckExpression::walkToExprPre(swift::Expr*) + 541
+test_crash_case '{nil-{_?'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | void (anonymous namespace)::visitProtocols<swift::ConformanceLookupTable::addProtocols(swift::NominalTypeDecl*, llvm::ArrayRef<swift::TypeLoc>, swift::ConformanceLookupTable::ConformanceSource, …
+test_crash_case '{(_>"",{'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::TypeBase::isEqual(swift::Type) + 15
+test_crash_case '{_{[true'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ConformanceLookupTable::resolveConformances(swift::NominalTypeDecl*, swift::ProtocolDecl*, swift::LazyResolver*) + 743
+test_crash_case '&.f{}()do'   # |   9 |     |     |     |     |  ✘  | std::__1::__function::__func<swift::constraints::ConstraintSystem::simplifyType(swift::Type, llvm::SmallPtrSet<swift::TypeVariableType*, 16u>&)::$_6, std::__1::allocator<swift::constraints::…
+test_crash_case '&Range.T{'   # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::getTypeOfMemberReference(swift::Type, swift::ValueDecl*, bool, bool, swift::constraints::ConstraintLocatorBuilder, swift::DeclRefExpr const*, …
+test_crash_case '&_{Range?'   # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::addConstraint(swift::constraints::Constraint*, bool, bool) + 280
+test_crash_case '.{nil<{\n{'  # |   9 |     |     |     |     |  ✘  | swift::ConformanceLookupTable::expandImpliedConformances(swift::NominalTypeDecl*, swift::DeclContext*, swift::LazyResolver*) + 496
+test_crash_case '{nil...{('   # |   9 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::simplifyMemberConstraint(swift::constraints::Constraint const&) + 416
+test_crash_case '&true{for{'  # |  10 |     |     |     |     |  ✘  | swift::AbstractClosureExpr::setParams(swift::Pattern*) + 80
+test_crash_case 'Range>\n&_{' # |  10 |     |     |     |     |  ✘  | swift::ASTContext::Allocate(unsigned long, unsigned int, swift::AllocationArena) const + 110
+test_crash_case '[&{}false?'  # |  10 |     |     |     |     |  ✘  | swift::constraints::ConstraintSystem::solve(llvm::SmallVectorImpl<swift::constraints::Solution>&, swift::FreeTypeVariableBinding) + 66
+test_crash_case '{nil{true?'  # |  10 |     |     |     |     |  ✘  | swift::constraints::ConstraintGraphNode::addConstraint(swift::constraints::Constraint*) + 109
+                              # +-----+-----+-----+-----+-----+-----+
 
 echo
 echo "Do you have older Xcode versions installed? If so, please run this script"
