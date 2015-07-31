48c163ade02e303a03895cd1656b95f1567d9481
practicalswift <practicalswift@users.noreply.github.com>
2015-07-31 22:05:38 +0200

    Add another golfer (7 chars).

--- swiftc_golf.sh
+++ swiftc_golf.sh
@@ -59,6 +59,7 @@ test_crash_case '()=()'       # |   5 |  ✓  |  ✓  |  ✘  |  ✘  |  ✘  | 
 test_crash_case 'for{{'       # |   5 |  ✓  |  ✓  |  ✘  |  ✘  |  ✘  |  ✓  | –
 test_crash_case '{&(&)'       # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ModuleFile::getDecl(llvm::Fixnum<31u, unsigned int>, llvm::Optional<swift::DeclContext*>) + 14041
 test_crash_case '({[({_'      # |   6 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | (anonymous namespace)::PreCheckExpression::walkToExprPre(swift::Expr*) + 541
+test_crash_case '&(<)==('     # |   7 |     |     |     |     |     |  ✘  | swift::ProtocolType::canonicalizeProtocols(llvm::SmallVectorImpl<swift::ProtocolDecl*>&) + 1598
 test_crash_case '{(_>"",{'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::TypeBase::isEqual(swift::Type) + 15
 test_crash_case '{_{[true'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | swift::ConformanceLookupTable::resolveConformances(swift::NominalTypeDecl*, swift::ProtocolDecl*, swift::LazyResolver*) + 743
 test_crash_case '{nil-{_?'    # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  | void (anonymous namespace)::visitProtocols<swift::ConformanceLookupTable::addProtocols(swift::NominalTypeDecl*, llvm::ArrayRef<swift::TypeLoc>, swift::ConformanceLookupTable::ConformanceSource, …
