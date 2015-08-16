f120730425ea8c43e7e38b170e0881a3e984cf1b
practicalswift <practicalswift@users.noreply.github.com>
2015-08-16 20:51:20 +0200

    Delete non-crash.

--- crashes/26808-enum-with-indirect-and-function.swift
+++
@@ -1,7 +0,0 @@
-// Distributed under the terms of the MIT license
-// Test case submitted to project by Andrew Bennett http:/github.com/therealbnut (@therealbnut)
-
-enum E {
-    indirect case C(E)
-    func f(){}
-}
