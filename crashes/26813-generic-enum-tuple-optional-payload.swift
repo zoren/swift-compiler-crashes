a1688d7057fba292e1ee1e3f0282fdd1e6988a10
Austin Zheng <austinzheng@gmail.com>
2015-09-08 19:09:29 -0700

    Adding crash related to generic enums with tuple payloads

---
+++ crashes/26813-generic-enum-tuple-optional-payload.swift
@@ -0,0 +1,11 @@
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/austinzheng (Austin Zheng)
+
+enum A<T> {
+  case Just(T)
+  case Error
+}
+
+func foo() -> A<(String, String?)> {
+  return A.Just("abc", "def")
+}
