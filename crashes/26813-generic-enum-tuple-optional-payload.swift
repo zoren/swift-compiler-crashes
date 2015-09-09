31367f9f09d2e696678c86c6f28390938bd22743
Austin Zheng <austinzheng@gmail.com>
2015-09-08 19:12:20 -0700

    Accidentally removed 'import Foundation'; adding it back in

--- crashes/26813-generic-enum-tuple-optional-payload.swift
+++ crashes/26813-generic-enum-tuple-optional-payload.swift
@@ -1,6 +1,8 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/austinzheng (Austin Zheng)
 
+import Foundation
+
 enum A<T> {
   case Just(T)
   case Error
