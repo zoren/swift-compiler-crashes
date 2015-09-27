bb9c7494790c7d625079e3dbd7dabf3ab99571d7
practicalswift <practicalswift@users.noreply.github.com>
2015-09-27 20:42:38 +0200

    Minimize crash case.

--- crashes/27816-control-flow-pattern-match-tuples.swift
+++ crashes/27816-control-flow-pattern-match-tuples.swift
@@ -1,32 +1,9 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/austinzheng (Austin Zheng)
 
-import Foundation
-
 let a = true
 let b = true
-
-func matchUsingIf() {
+func c() {
     if case (true, true) = (a, b) {
-        return
-    }
-}
-
-func matchUsingGuard() {
-    guard case (true, true) = (a, b) else {
-        return
-    }
-}
-
-func matchUsingForIn() {
-    let c = [(true, true), (true, false), (false, false), (true, true)]
-    for case (true, true) in c {
-        // do something
-    }
-}
-
-func matchUsingWhile() {
-    while case (true, true) = (a, b) {
-        break
     }
 }
