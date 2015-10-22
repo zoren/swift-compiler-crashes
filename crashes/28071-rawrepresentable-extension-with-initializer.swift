5342597e54dc540a821339e820c10ca3395c2c14
Andrii Chernenko <andrii.chernenko@nexthub.com>
2015-10-22 12:33:38 +0200

    added RawRepresentable extension crash

---
+++ crashes/28071-rawrepresentable-extension-with-initializer.swift
@@ -0,0 +1,14 @@
+
+extension RawRepresentable {
+    
+    init?(rawValue optionalRawValue: RawValue?) {
+        guard let rawValue = optionalRawValue, value = Self(rawValue: rawValue) else { return nil }
+        self = value
+    }
+}
+
+enum E: Int {
+    case A = 0, B
+}
+
+let v: E = .A
