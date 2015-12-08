dee685e160b404a39b1d77a5a2646d71db021112
practicalswift <practicalswift@users.noreply.github.com>
2015-12-08 20:36:45 +0100

    Add crashes. Update status for Swift version 2.2-dev (6bfbe1b49e).

--- swiftc_golf.sh
+++ swiftc_golf.sh
@@ -47,51 +47,62 @@ test_crash_case() {
   else
     echo "· ✓ · ${escaped_source_code} (${number_of_bytes} bytes)"
   fi
+  # egrep 0x <<< "${compilation_output}" | egrep 'swift::' | head -1
+  # echo
 }
-                                  # +-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-------+----------------+
-                                  # | len | 6.2 | 6.4 | 7b1 | 7b2 | 7b3 | 7b4 | 7b5 | 7b6 | 7.1b1 | 7.1b2 | 7.1b3 | Crash location |
-                                  # +-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-------+----------------+
-test_crash_case '&_'              # |   2 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✓  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(…)
-test_crash_case '&0{'             # |   3 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ModuleFile::getDecl(…)
-test_crash_case '{&_'             # |   3 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✓  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::diagnoseFailureForExpr(…)
-test_crash_case '&(-_'            # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::matchTypes(…)
-test_crash_case '&[_?'            # |   4 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::Type::walk(…)
-test_crash_case '&(>,_'           # |   5 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ModuleFile::getType(…)
-test_crash_case '&[(-{'           # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::StreamPrinter::printText(…)
-test_crash_case '()=()'           # |   5 |  ✓  |  ✓  |  ✘  |  ✘  |  ✘  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | emitSimpleAssignment(…)
-test_crash_case 'for{{'           # |   5 |  ✓  |  ✓  |  ✘  |  ✘  |  ✘  |  ✓  |  ✓  |  ✓  |   ✓   |   ✓   |   ✓   | –
-test_crash_case '{$0()'           # |   5 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | (anonymous namespace)::UncurriedCandidate::getUncurriedFunctionType()
-test_crash_case '{&(&)'           # |   5 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ModuleFile::getDecl(…)
-test_crash_case '{[-{_'           # |   5 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::solveRec(…)
-test_crash_case '{_>{{'           # |   5 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::simplifyType(…)
-test_crash_case '&_{Int'          # |   6 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::GenericSignature::get(…)
-test_crash_case '(&.f>_'          # |   6 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::GenericParamList::addNestedArchetypes(…)
-test_crash_case '({[({_'          # |   6 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | (anonymous namespace)::PreCheckExpression::walkToExprPre(…)
-test_crash_case '[_?,&_'          # |   6 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | std::__1::__function::__func<swift::constraints::ConstraintSystem::simplifyType(…)
-test_crash_case '&(<)==('         # |   7 |     |     |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ProtocolType::canonicalizeProtocols(…)
-test_crash_case '&(Int:_'         # |   7 |     |     |     |     |     |     |     |     |       |   ✘   |   ✘   | swift::ASTVisitor<…>::visit(…)
-test_crash_case ']{&[]()'         # |   7 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintGraph::gatherConstraints(…)
-test_crash_case 'nil as('         # |   7 |     |     |     |     |     |     |     |  ✘  |   ✘   |   ✓   |   ✓   | swift::Type::transform(…)
-test_crash_case '{(&_{("'         # |   7 |     |     |     |     |     |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::matchTypes(…)
-test_crash_case '{[ &{}{'         # |   7 |     |     |     |     |     |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ArchetypeType::setNestedTypes(…)
-test_crash_case '{f\n-{{_'        # |   7 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | (anonymous namespace)::PreCheckExpression::walkToExprPre(…)
-test_crash_case '&_{Array'        # |   8 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::getConstraintLocator(…)
-test_crash_case '[&nil,_?'        # |   8 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::Type::transform(…)
-test_crash_case 'do{&{}{)'        # |   8 |     |     |     |     |     |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | (anonymous namespace)::RawMemoryObject::readBytes(…)
-test_crash_case 'true[&{_'        # |   8 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::GenericParamList::create(…)
-test_crash_case '{(_>"",{'        # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✓  |  ✓  |   ✓   |   ✓   |   ✓   | swift::TypeBase::isEqual(…)
-test_crash_case '{_{[true'        # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ConformanceLookupTable::resolveConformances(…)
-test_crash_case '{nil-{_?'        # |   8 |  ✓  |  ✓  |  ✓  |  ✓  |  ✓  |  ✘  |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | void (anonymous namespace)::visitProtocols<swift::ConformanceLookupTable::addProtocols(…)
-test_crash_case '&.f{}()do'       # |   9 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | std::__1::__function::__func<swift::constraints::ConstraintSystem::simplifyType(…)
-test_crash_case '&Range.T{'       # |   9 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::getTypeOfMemberReference(…)
-test_crash_case '&_{Range?'       # |   9 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::addConstraint(…)
-test_crash_case '.{nil<{\n{'      # |   9 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ConformanceLookupTable::expandImpliedConformances(…)
-test_crash_case 'Array([[]'       # |   9 |     |     |     |     |     |     |     |     |       |   ✘   |   ✓   | swift::constraints::ConstraintGraph::computeConnectedComponents(…)
-test_crash_case 'nil?=\n&_,'      # |   9 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::LValueType::get(…)
-test_crash_case 'var(()...'       # |   9 |     |     |     |     |     |     |  ✘  |  ✓  |   ✓   |   ✓   |   ✓   | swift::TypeChecker::typeCheckPattern(…)
-test_crash_case '{nil...{('       # |   9 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::simplifyMemberConstraint(…)
-test_crash_case '&true{for{'      # |  10 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::AbstractClosureExpr::setParams(…)
-test_crash_case 'Range>\n&_{'     # |  10 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::ASTContext::Allocate(…)
-test_crash_case '[&{}false?'      # |  10 |     |  ✓  |     |     |     |  ✘  |  ✓  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintSystem::solve(…)
-test_crash_case '{nil{true?'      # |  10 |     |  ✓  |     |     |     |  ✘  |  D  |  ✓  |   ✓   |   ✓   |   ✓   | swift::constraints::ConstraintGraphNode::addConstraint(…)
-                                  # +-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-------+
+
+
+echo "Crashing:"
+
+                                  # +-----+----------------+
+                                  # | len | Crash location |
+                                  # +-----+----------------+
+test_crash_case 'Array([[]'       # |   9 | swift::constraints::ConstraintGraphNode::getAdjacency(swift::TypeVariableType*)
+test_crash_case '[_?,&_'          # |   6 | swift::constraints::ConstraintGraph::bindTypeVariable(swift::TypeVariableType*, swift::Type) [alternative: '[&nil,_?']
+test_crash_case 'nil?=\n&_,'      # |   9 | swift::LValueType::get(swift::Type)
+test_crash_case 'var(()...'       # |   9 | swift::Decl::walk(swift::ASTWalker&)
+test_crash_case '{map($0'         # |   7 | swift::Expr::walk(swift::ASTWalker&) [alternative: '{}{{d nil']
+                                  # +-----+
+
+echo
+echo "Fixed:"
+
+test_crash_case '&(-_'
+test_crash_case '&(<)==('
+test_crash_case '&(>,_'
+test_crash_case '&(Int:_'
+test_crash_case '&.f{}()do'
+test_crash_case '&0{'
+test_crash_case '&Range.T{'
+test_crash_case '&[(-{'
+test_crash_case '&[_?'
+test_crash_case '&_'
+test_crash_case '&_{Array'
+test_crash_case '&_{Int'
+test_crash_case '&_{Range?'
+test_crash_case '&true{for{'
+test_crash_case '(&.f>_'
+test_crash_case '()=()'
+test_crash_case '({[({_'
+test_crash_case '.{nil<{\n{'
+test_crash_case 'Range>\n&_{'
+test_crash_case '[&{}false?'
+test_crash_case ']{&[]()'
+test_crash_case '_={$0()}'
+test_crash_case 'do{&{}{)'
+test_crash_case 'for{{'
+test_crash_case 'nil as('
+test_crash_case 'true[&{_'
+test_crash_case '{$0()'
+test_crash_case '{&(&)'
+test_crash_case '{&_'
+test_crash_case '{(&_{("'
+test_crash_case '{(_>"",{'
+test_crash_case '{[ &{}{'
+test_crash_case '{[-{_'
+test_crash_case '{_>{{'
+test_crash_case '{_{[true'
+test_crash_case '{f\n-{{_'
+test_crash_case '{nil-{_?'
+test_crash_case '{nil...{('
+test_crash_case '{nil{true?'
