f308b7b63aac869ddbc689c2cb2cfcc0b8f838e2
Ben Shanfelder <benshan@gmail.com>
2015-12-07 22:25:01 -0500

    Add crash for missing getter in extension property.

---
+++ crashes/28184-swift-constraints-constraintsystem-gettypeofmemberreference.swift
@@ -0,0 +1,11 @@
+// Distributed under the terms of the MIT license
+// Test case found by https://github.com/benshan (Ben Shanfelder)
+
+import Foundation
+
+extension NSObject {
+    var handler: Int {
+        // FIXME: Crashing due to lack of get {}
+        set {}
+    }
+}
