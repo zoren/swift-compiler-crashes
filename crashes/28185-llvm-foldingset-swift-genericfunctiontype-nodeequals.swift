5eedd0a5058b8cc18939485bb183479d4e982012
Ben Shanfelder <benshan@gmail.com>
2015-12-10 22:53:09 -0500

    Add crash for missing getter in protocol when a class with the getter is extended via the protocol

---
+++ crashes/28185-llvm-foldingset-swift-genericfunctiontype-nodeequals.swift
@@ -0,0 +1,13 @@
+// Distributed under the terms of the MIT license
+// Test case found by https://github.com/benshan (Ben Shanfelder)
+
+import Cocoa
+
+protocol A: class {
+    var target: AnyObject? { set } // FIXME: Missing "get" wrt extension on NSControl leads to crash.
+}
+
+class B: NSObject {
+}
+
+extension NSControl: A {}
