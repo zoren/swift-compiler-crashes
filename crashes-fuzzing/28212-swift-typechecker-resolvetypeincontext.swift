808cb90d7fdf1de3d4f3a7da2fad0bca5f84bfae
practicalswift <practicalswift@users.noreply.github.com>
2016-01-28 10:40:14 +0100

    Add annotations.

--- crashes-fuzzing/28212-swift-typechecker-resolvetypeincontext.swift
+++ crashes-fuzzing/28212-swift-typechecker-resolvetypeincontext.swift
@@ -1,3 +1,10 @@
+// RUN: not --crash %target-swift-frontend %s -parse
+// REQUIRES: asserts
+
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
 struct B{let f= <a
 extension{
 protocol A{
