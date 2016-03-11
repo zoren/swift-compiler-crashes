a92ff44e904732b34562f4d70b659ea062dade6f
practicalswift <practicalswift@users.noreply.github.com>
2016-03-11 14:34:51 +0100

    Add crash case crashes-fuzzing/28263-swift-typechecker-validatedecl

---
+++ crashes-fuzzing/28263-swift-typechecker-validatedecl.swift
@@ -0,0 +1,15 @@
+// RUN: not --crash %target-swift-frontend %s -parse
+// REQUIRES: asserts
+
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+protocol e{func^
+protocol P{
+extension{
+class A{
+protocol c:e
+protocol P{
+typealias e:c{
+}func^
