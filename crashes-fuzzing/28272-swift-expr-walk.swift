250996c0a6c93ea7745b9aeb94a082b12c6a23f7
practicalswift <practicalswift@users.noreply.github.com>
2016-03-15 11:48:04 +0100

    Add crash case crashes-fuzzing/28272-swift-expr-walk

---
+++ crashes-fuzzing/28272-swift-expr-walk.swift
@@ -0,0 +1,6 @@
+// This source file is part of the Swift.org open source project
+// See http://swift.org/LICENSE.txt for license information
+
+// RUN: not --crash %target-swift-frontend %s -parse
+// REQUIRES: asserts
+.A[
