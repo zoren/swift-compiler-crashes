000d464976faf21b8a5b8a6f981e1bbd77b2a6c6
practicalswift <practicalswift@users.noreply.github.com>
2016-03-16 10:58:28 +0100

    Add crash case crashes-fuzzing/28273-swift-archetypebuilder-potentialarchetype-gettype

---
+++ crashes-fuzzing/28273-swift-archetypebuilder-potentialarchetype-gettype.swift
@@ -0,0 +1,7 @@
+// This source file is part of the Swift.org open source project
+// See http://swift.org/LICENSE.txt for license information
+
+// RUN: not --crash %target-swift-frontend %s -parse
+enum S<T where S.e=p{
+struct e<T:a
+class a
